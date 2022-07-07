f5594260fb877a9198887c627019d09809c68cbb
Akram Adib <md.akramuladib2@gmail.com>
2022-07-07 19:25:36 +0600

    C:/Program Files/Git/

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'leanne.graham@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jonny English',
+        username: 'English',
+        email: 'jonnybro@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Dwayne Johnson',
+        username: 'Rock',
+        email: 'TheRock@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Dalai lama',
+        username: 'Monk',
+        email: 'meditation@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Doctor Strange',
+        username: 'Shephen',
+        email: 'magic@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Jhon Wick',
+        username: 'BabaYaga',
+        email: 'hitman@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Tony Stark',
+        username: 'genius',
+        email: 'playboy@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Nick Fury',
+        username: 'one eye',
+        email: 'shildMan@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Jhon F. Cenedy',
+        username: 'Leader',
+        email: 'president@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Vlademir Putin',
+        username: 'Mr. Nuke',
+        email: 'rediation@gmail.com'
+    }
+]
